a5651afce7489614ab5ea5a9eb96faf9928b0a15
Mihajlo <djmixa26.hockey@gmail.com>
2023-09-16 16:24:37 +0200

    Icon for app created, a splash screen implemented with the new icon.

--- app/src/main/java/com/mixa/dictio/presentation/view/activities/MainActivity.kt
+++ app/src/main/java/com/mixa/dictio/presentation/view/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mixa.dictio.presentation.view.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.mixa.dictio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(){
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
